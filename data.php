de8a9807424cf70c5145f27d977e87e2343a6faf
Luca <mylince@gmail.com>
2020-04-20 15:31:08 +0200

    Fatte prove con method in index.php (ciclo foreach e for).

---
+++ data.php
@@ -0,0 +1,70 @@
+<!-- Creo il mio array di oggetti che mi servirà per compilare il mio index
+1) +++++++ Posso utilizzarlo senza server.php direttamente sull'index, richiamando il mio "data.php" con include, e ciclare li l'array che mi serve +++++++
+2) +++++++ Posso richiamrlo sul mio server.php con include, e poi passare in URL il mio server.php per ciclare il mio array ++++++
+-->
+<?php
+
+   $playlist = [
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ],
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ],
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ],
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ],
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ],
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ],
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ],
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ],
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ],
+       [
+           'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2019/06/Jova-Beach-Party-copertina-album-jovanotti.jpg',
+           'title' => 'Iron',
+           'author' => 'Luca',
+           'year' => '2019'
+       ]
+   ];
+
+ ?>
--- index.php
+++ index.php
@@ -7,13 +7,43 @@
         <script src="https://kit.fontawesome.com/4cd7baf8bb.js" crossorigin="anonymous"></script>
         <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.3/handlebars.min.js"></script>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="public/css/style.css">
     </head>
     <body>
 
+    <!-- Test con metodo senza utilizzo di Ajax e Server.php -->
+        <?php
+            include 'data.php';
+        ?>
 
+    <!-- Con ciclo foreach -->
+        <!-- <?php foreach ($playlist as $key => $album) { ?>
+            <div class="card">
+                <img src=" <?php echo $album['poster']; ?> ">
+                <h2> <?php echo $album['title']; ?> </h2>
+                <p> <?php echo $album['author']; ?> </p>
+                <small> <?php echo $album['year']; ?> </small>
+            </div>
+        <?php } ?> -->
+
+    <!-- Con ciclo for -->
+        <?php for ($i=0; $i < count($playlist) ; $i++) { ?>
+            <?php $album = $playlist[$i]; ?>
+            <div class="card">
+                <img src=" <?php echo $album['poster']; ?> ">
+                <h2> <?php echo $album['title']; ?> </h2>
+                <p> <?php echo $album['author']; ?> </p>
+                <small> <?php echo $album['year']; ?> </small>
+            </div>
+
+        <?php } ?>
+
+
+        <script id="template" type="text/x-handlebars-template">
+            <img src="{{img}}">
+        </script>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script src="js/main.js" charset="utf-8"></script>
+        <script src="public/js/app.js" charset="utf-8"></script>
     </body>
 </html>
